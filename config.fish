1a8a9dd03a810e03fd406789d5cfcfc629eb4cfd
Kristof Jakab <jaksi@users.noreply.github.com>
2023-03-10 12:15:25 +0000

    stuff

--- config.fish
+++ config.fish
@@ -1,11 +1,11 @@
-if status is-interactive
+if status is-interactive; and not set -q TMUX
     set -l uptime
     if test (uname -s) = 'Darwin'
-        set uptime (math (date +%s)"-"(sysctl -n kern.boottime | cut -d ' ' -f 4 | cut -d , -f 1))
+        set uptime (math (date +%s)'-'(sysctl -n kern.boottime | cut -d ' ' -f 4 | cut -d , -f 1))
     else
         set uptime (cut -d ' ' -f 1 /proc/uptime)
     end
-    if test $uptime -gt 432000; and read --nchars 1 -l response --prompt-str="Up more than 5 days. Reboot? (y) "; and test $response = "y"
+    if test $uptime -gt 432000; and read --nchars 1 -l response --prompt-str='Up more than 5 days. Reboot? (y) '; and test $response = 'y'
         if test (uname -s) = 'Darwin'
             osascript -e 'tell app "System Events" to restart'
         else
